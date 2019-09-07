237264416243348dbcb0c1c9f72096aa218c4af4
Ceejaymar <martinezcj2@gmail.com>
2019-09-07 02:42:43 -0400

    Portfolio build new website

---
+++ precache-manifest.37d5bb6b1031b4ffcd07431ef0bf73ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a46a0c6ba565715400fda8b8c90e803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e16dacb2c51021bcff",
+    "url": "/static/css/main.474a4481.chunk.css"
+  },
+  {
+    "revision": "2bafa21b1fa0553478c6",
+    "url": "/static/js/2.5742ff76.chunk.js"
+  },
+  {
+    "revision": "47e16dacb2c51021bcff",
+    "url": "/static/js/main.206949e2.chunk.js"
+  },
+  {
+    "revision": "246661b5def607c76682",
+    "url": "/static/js/runtime~main.ea03829a.js"
+  },
+  {
+    "revision": "da8894ea375c2e320518c40919375b84",
+    "url": "/static/media/bg.da8894ea.svg"
+  },
+  {
+    "revision": "96e054e94d5d8597ca6594bbda1748fe",
+    "url": "/static/media/carlos3.96e054e9.png"
+  }
+]);
